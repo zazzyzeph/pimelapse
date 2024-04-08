c86c93a963d7162a5841e1a462a16844562659e8
Zephyr Prusinski <zephyr@zephnet.biz>
2024-04-08 11:32:14 -0400

    adds gitignore for created files, updates camera script to use libcamera-jpeg, removes junk mp4 file

--- camera.sh
+++ camera.sh
@@ -3,4 +3,4 @@
 PIMELAPSE_DIR=$HOME/pimelapse
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-libcamera-still --width 1920 --height 1080 --awb daylight --ev -1 -q 100 -n -o $PIMELAPSE_DIR/images/$DATE.jpg 
+libcamera-jpeg --width 1920 --height 1080 --awb daylight --ev -1 -q 100 -n -o $PIMELAPSE_DIR/images/$DATE.jpg 
